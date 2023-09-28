f87b9f9b2847694ebc8100dd8f96b557ef31d516
Gustavo Rajab <122064946+gustajab@users.noreply.github.com>
2023-09-28 14:28:41 -0300

    GO

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import "fmt"
 
-var nome string = "gustavo"
-
+//var nome string = "gustavo"
 func main() {
+	var num1, num2 int
+
+    fmt.Print("Digite o primeiro número: ")
+    fmt.Scanln(&num1)
+
+    fmt.Print("Digite o segundo número: ")
+    fmt.Scanln(&num2)
+
+    resultado := num1 + num2
+
+    fmt.Printf("A soma de %d e %d é igual a %d\n", num1, num2, resultado)
 
 	//len("ALO MUNDO")
-	fmt.Println("ola meu nome é RAJAB, " + nome)
+	//fmt.Println("ola meu nome é", nome)
 
 }
